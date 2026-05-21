160533e848b44e6c496e40b03ee093a48e1b63ae
William Lam <williamntlam@gmail.com>
2026-05-20 22:20:22 -0400

    feat: define node struct for linked list

--- deque.go
+++ deque.go
@@ -3,8 +3,8 @@ package distributeddeque
 import "context"
 
 type Deque interface {
-	PushFront(ctx context.Context, value[]byte) error
-	PushBack(ctx context.Context, value[]byte) error
+	PushFront(ctx context.Context, value []byte) error
+	PushBack(ctx context.Context, value []byte) error
 	PopFront(ctx context.Context) ([]byte, error)
 	PopBack(ctx context.Context) ([]byte, error)
 	Len(ctx context.Context) (int64, error)
--- memory/node.go
+++ memory/node.go
@@ -1,8 +1,7 @@
 package memory
 
-// node is one element in the doubly-linked list backing MemoryDeque.
 type node struct {
 	value []byte
-	prev  *node
-	next  *node
+	prev *node
+	next *node
 }
