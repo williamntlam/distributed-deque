9c9d2b27a652f8ff1c9ed5c282ca802e4520454d
William Lam <williamntlam@gmail.com>
2026-05-24 18:12:29 -0400

    feat: implement deque.go methods

--- memory/deque.go
+++ memory/deque.go
@@ -72,17 +72,98 @@ func (d *MemoryDeque) PushBack(ctx context.Context, value []byte) error {
 
 func (d *MemoryDeque) PushFront(ctx context.Context, value []byte) error {
 
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	if err := d.requireOpen(); err != nil {
+		return err
+	}
+
+	newNode := &node{value: value}
+
+	if d.head == nil {
+
+		d.head = newNode
+		d.tail = newNode
+
+	} else {
+
+		newNode.next = d.head
+		d.head.prev = newNode
+		d.head = newNode
+
+	}
+
+	d.size++
+
+	return nil
+
 }
 
 func (d *MemoryDeque) PopFront(ctx context.Context) ([]byte, error) {
+
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	if err := d.requireOpen(); err != nil {
+		return nil, err
+	}
+
+	if d.head == nil {
+		return nil, distributeddeque.ErrEmpty
+	}
+
+	frontNode := d.head
+	d.head = frontNode.next
+
+	if d.head != nil {
+		d.head.prev = nil
+	} else {
+		d.tail = nil
+	}
+
+	d.size--
+
+	return frontNode.value, nil
 
 }
 
 func (d *MemoryDeque) PopBack(ctx context.Context) ([]byte, error) {
 
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	if err := d.requireOpen(); err != nil {
+		return nil, err
+	}
+
+	if d.tail == nil {
+		return nil, distributeddeque.ErrEmpty
+	}
+
+	backNode := d.tail
+	d.tail = backNode.prev
+	if d.tail != nil {
+		d.tail.next = nil
+	} else {
+		d.head = nil
+	}
+
+	d.size -= 1
+
+	return backNode.value, nil
+
 }
 
-func (d *MemoryDeque) Close() error {  }
+func (d *MemoryDeque) Close() error { 
+
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	d.closed = true
+	
+	return nil
+}
 
 var _ distributeddeque.Deque = (*MemoryDeque)(nil)
 
