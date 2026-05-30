7a02ab510c3f863aa4d106e6710c3fe0e8d22fb7
William Lam <williamntlam@gmail.com>
2026-05-29 21:29:12 -0400

    feat: added handlePush and handlePop function def

--- cmd/queued/main.go
+++ cmd/queued/main.go
@@ -1,8 +1,17 @@
 package main
 
 import (
+	"errors"
+	"io"
+	"log"
 	"net/http"
+	dq "github.com/williamntlam/distributed-deque"
 	"github.com/williamntlam/distributed-deque/memory"
+)
+
+const (
+	addr         = ":8080"
+	maxBodyBytes = 1 << 20 // 1 MiB
 )
 
 type server struct {
@@ -16,4 +25,13 @@ func newServer() *server {
 func (s *server) registerRoutes(mux *http.ServeMux) {
 	mux.HandleFunc("POST /push", s.handlePush)
 	mux.HandleFunc("GET /pop", s.handlePop)
+}
+
+func (s *server) handlePush(w http.ResponseWriter, r *http.Request) {
+
+}
+
+
+func (s *server) handlePop(w http.ResponseWriter, r *http.Request) {
+
 }
